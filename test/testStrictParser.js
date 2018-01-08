5e3036de94631ee3724fcecad8a5484ee04507fd
Vivek Pandey <vivekpandey1596@gmail.com>
2018-01-08 13:20:25 +0530

    31 test are passing but i havn't done with assert.throws fn

--- test/testStrictParser.js
+++ test/testStrictParser.js
@@ -1,7 +1,7 @@
 const src=function(filePath){return "../src/"+filePath};
 const errors=function(filePath){return "../src/errors/"+filePath};
 
-const assert=require('assert');
+const assert=require('chai').assert;
 const StrictParser=require(src('index.js')).StrictParser;
 const InvalidKeyError=require(errors('invalidKeyError.js'));
 
